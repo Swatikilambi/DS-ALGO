55a3f378cf0eb4ff1829f913796cf9c0af059f45
Swatikilambi <126923839+Swatikilambi@users.noreply.github.com>
2023-03-26 23:25:23 -0400

    Update ExcelUtils.java

--- src/test/java/utilities/ExcelUtils.java
+++ src/test/java/utilities/ExcelUtils.java
@@ -10,32 +10,6 @@ public class ExcelUtils {
 	
 	static int rowcount;
 	
-	//int j=0;
-	/*public static void main(String args[])
-	{
-		ExcelUtils a= new ExcelUtils();
-		a.GetRowcount();
-		GetLoginCredentials("Sheet1",1,0);
-		//a.GetPassword();
-	}*/
-	/*public void getRowcount()
-	{
-		try
-		{
-			
-			String prjectpath = System.getProperty("user.dir");
-			XSSFWorkbook workbook = new XSSFWorkbook(prjectpath +"\\Excel\\LoginDetails.xlsx");
-			XSSFSheet sheet = workbook.getSheet("Sheet1");
-			rowcount= sheet.getPhysicalNumberOfRows();
-			System.out.println("Number of Rows: " + rowcount);
-		}
-		 catch (IOException e) {
-				System.out.println(e.getMessage());
-				e.getCause();
-				e.printStackTrace();
-			}
-		}*/
-
 	public static String getLoginCredentials(String sheetName,int rowNumber)
 	{
 		String username = null;
